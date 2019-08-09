e552d0a02113662f671ba2943e06e2e93f1dce0c
Dave Cooper <dave@davecooper.org>
2019-08-09 12:30:08 +0100

    trying to get this to work on codesandbox

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import "core-js/es/reflect";
+import "core-js/stable/reflect";
+import "core-js/features/reflect";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));
